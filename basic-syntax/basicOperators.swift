34f2d5d697b11647d06a9c8815e24bece335189e
Venujan Satchithanantham <venujan.satchithanantham@uoit.net>
2018-12-10 09:47:16 -0500

    Add files via upload

---
+++ basic-syntax/basicOperators.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+//adding integers together
+var apples: Int = 3
+var oranges: Int = 5
+var fruits: Int = apples + oranges 
+
+print (fruits)
+
+//concatenating words
+var firstWord = "My name"
+var secondWord = "is Joe"
+var sentence = firstWord + " " + secondWord
+
+print (sentence)
+
+//using boolean operators 
+if apples == oranges {
+    print ("True")
+}
+    else {
+       print ("False")
+}
